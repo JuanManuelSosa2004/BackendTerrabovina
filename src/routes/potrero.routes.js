989fbe6b5d832b44f214ed52466918f876d9f94e
IgnaBilli <ignabilli03@gmail.com>
2026-09-22 10:10:22 -0300

    Remove intrapaddock API and background analysis integration

--- src/routes/potrero.routes.js
+++ src/routes/potrero.routes.js
@@ -4,7 +4,6 @@ const express = require('express');
 const potreroController = require('../controllers/potrero.controller');
 const ganadoController = require('../controllers/ganado.controller');
 const ndviClimaController = require('../controllers/ndviClima.controller');
-const intrapotreroController = require('../controllers/intrapotrero.controller');
 const estimacionController = require('../controllers/estimacion.controller');
 const recomendacionController = require('../controllers/recomendacion.controller');
 const trasladoGanadoController = require('../controllers/trasladoGanado.controller');
@@ -30,8 +29,6 @@ router.get('/:potreroId/ganado', ganadoController.listByPotrero);
 
 // #26, #27, #28
 router.get('/:potreroId/ndvi', ndviClimaController.getNdviVigente);
-router.get('/:potreroId/intrapotrero', intrapotreroController.get);
-router.post('/:potreroId/intrapotrero', intrapotreroController.refresh);
 router.get('/:potreroId/ndvi/consulta-rango', ndviClimaController.getNdviHistorico);
 router.get('/:potreroId/dato-clima', ndviClimaController.getDatoClimaVigente);
 
